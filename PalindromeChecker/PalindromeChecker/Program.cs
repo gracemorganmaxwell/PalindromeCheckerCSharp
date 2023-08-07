e5ab131486d07bfa89d60e9818980209df9f17e7
grace-morganmaxwell <gracemorganmaxwell@gmail.com>
2023-08-07 18:30:44 +1200

    Add files via upload
    Tested and worked, created locally using Visual Studio 2022 and C # version 7.0.

---
+++ PalindromeChecker/PalindromeChecker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PalindromeChecker
+{
+    public static class Checker
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to our Palindrome Checker in C#");
+            Console.WriteLine("Please input a word to be checked:");
+
+            string input = Console.ReadLine();
+
+            // Removing case sensitivity for comparison
+            string lowerCaseInput = input.ToLower();
+
+            // Reversing the string
+            string reversed = new string(lowerCaseInput.Reverse().ToArray());
+
+            if (lowerCaseInput == reversed)
+            {
+                Console.WriteLine("Valid, yes that's a Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("Not Valid, a Palindrome is a word that is spelt the same forwards and backwards; please try again.");
+            }
+        }
+    }
+}
